a818f52ee0cd881ab15b806ff46a7ce3fc08bb0c
zmpeg <zandstra.matt@gmail.com>
2017-04-05 10:47:49 -0600

    more tests for internal get request func

--- test/test_helper.js
+++ test/test_helper.js
@@ -7,16 +7,4 @@ process.env.TZ = 'America/Edmonton'
 // Suppress logging messages
 process.env.LOG_LEVEL = 'error'
 
-const FMECloudAPI = require('../lib/fme_cloud_api');
-const original = Object.assign({}, FMECloudAPI);
-
-FMECloudAPI.stub = function(key, fn) {
-    _.set(this, key, fn.bind(this));
-};
-
-FMECloudAPI.unstub = function(key) {
-    if(key) _.set(this, key, original[key].bind(this));
-    else Object.assign(this, original);
-};
-
-module.exports = FMECloudAPI;
+module.exports = require('../lib/fme_cloud_api');
---
+++ test/unit/get.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const FMECloudAPI = require('../test_helper');
+const sinon = require('sinon');
+const PassThrough = require('stream').PassThrough;
+const http = require('http');
+
+describe('FMECloudAPI', function() {
+
+    describe('get()', function(){
+
+        beforeEach(function() {
+            this.request = sinon.stub(http, 'request');
+        });
+
+        afterEach(function() {
+            http.request.restore
+        });
+
+        it('converts the response to string', function() {
+            var expected = '/somepath';
+            var client = new FMECloudAPI();
+            var request = new PassThrough();
+            var response = new PassThrough();
+            response.write(expected);
+            response.end();
+            this.request.callsArgWith(1, response).returns(request);
+            return client.get(expected).then(function(result) {
+                assert.equal(result, expected)
+            });
+        });
+    });
+});
